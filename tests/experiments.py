b2214eabcff97c1a871852364ef41b484ed5f669
fteodoro803 <agustinteodoro803@gmail.com>
2024-04-12 18:58:30 +1000

    interface functionality testing

--- tests/experiments.py
+++ tests/experiments.py
@@ -4,19 +4,19 @@ import json
 import requests
 import ptv_api as ptv
 import utilities
-
+import functionality
 
 # Test Variables #
 
-# Hope Street
-latitude, longitude = -37.761665989492705, 144.9422826877412
-route_type = 1
-route_types = [1]
-route_id = 11529
-max_results = 5
-max_distance = 300
-stop_id = 3086
-direction_id = 21   # towards city
+# # Hope Street
+# latitude, longitude = -37.761665989492705, 144.9422826877412
+# route_type = 1
+# route_types = [1]
+# route_id = 11529
+# max_results = 5
+# max_distance = 300
+# stop_id = 3086
+# direction_id = 21   # towards city
 
 # # Queen Vic Market
 # latitude, longitude = -37.80607940323542, 144.95735362615287
@@ -24,11 +24,21 @@ direction_id = 21   # towards city
 # route_types = [1]
 # max_results = 5
 # max_distance = 300
+# stop_id = 2258
 # route_id = 725
 # route_ids = [725, 887, 897]     # 19, 57, 59
 
+# Melb Central
+latitude, longitude = -37.811359153253406, 144.96215876061984
+route_types = [0,1]
+max_results = 1
+max_distance = 500
+route_type = 1      # tram
+stop_id = 2718      # tram
+# route_type = 0      # train
+# stop_id = 1120      # train
 
-# Function Testing
+# API Testing #
 
 # # Get Route Types
 # route_types = ptv.routeTypes()
@@ -42,9 +52,18 @@ direction_id = 21   # towards city
 # print(f"Directions for Route {route_id}: \n{json.dumps(ptv.routeDirections(route_id).json(), indent=2)} \n")
 
 # Test Departures
-expands = None
-departures = ptv.departures(route_type, stop_id, route_id, direction_id=direction_id, max_results=5)    # JSON File
+# expands = None
+# expands = ["Direction", "Stop", "Route"]
+expands = ["Direction", "Disruption", "Stop", "Route", "VehicleDescription"]
+# expands = ["All"]
+# departures = ptv.departures(route_type, stop_id, route_id, max_results=3, expand=expands)    # JSON File
+departures = ptv.departures(route_type, stop_id, max_results=1, expand=expands)    # JSON File , no Direction ID, no Route ID
 new_departures = utilities.convertDepartureTimesToLocalTime(departures.json())
 print(f"Departures for stopID {stop_id}, routeType {route_type}: \n{json.dumps(new_departures, indent=4)} \n")
 
 
+# Widget Functionality Testing #
+
+# directions = functionality.stopRoutes(stops)
+
+departures3 = functionality.routeDepartures(new_departures)
