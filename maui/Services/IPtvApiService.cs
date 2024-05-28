58c2a9bf28623b4ae1779adec1b10dc5b795a092
fteodoro803 <agustinteodoro803@gmail.com>
2024-05-28 22:31:44 +1000

    cleaned code and fixed warnings

--- maui/Services/IPtvApiService.cs
+++ maui/Services/IPtvApiService.cs
@@ -3,12 +3,14 @@
 public interface IPtvApiService
 {
     // string GetUrl(string request);
-    string GetUrl(string request, List<(string, string)> parameters);
+    string GetUrl(string request, List<(string, string)>? parameters);
     Task<string> GetRouteTypes();
-    Task<string> GetRouteDirections(string routeId);      // should have a route id arg
+    Task<string> GetRouteDirections(string? routeId);      // should have a route id arg
 
-    Task<string> GetStops(string location, List<int>? routeTypes = null, int? maxResults = null,        // convert these into string types, then do conversion later
+    Task<string> GetStops(string? location, List<int>? routeTypes = null, int? maxResults = null,        // convert these into string types, then do conversion later
         int? maxDistance = null);
+    
+    // Task<string> GetDepartures()
     
     // test
     public string GetCurrentUrl();
--- maui/Services/PtvApiService.cs
+++ maui/Services/PtvApiService.cs
@@ -3,8 +3,6 @@ using Newtonsoft.Json;  // testing geting config.json
 
 using System.Text;
 using System.Web;
-using System.Collections.Generic;
-using System.Net.Http;
 using System.Security.Cryptography;
 using Newtonsoft.Json.Linq;
 
@@ -16,32 +14,27 @@ namespace maui.Services;
 public class PtvApiService : IPtvApiService
 {
     // API Credentials
-    private readonly IConfiguration _config;
-    private readonly string _userID;
+    // private readonly IConfiguration _config;
+    private readonly string _userId;
     private readonly string _apiKey;
     
     // Test Values
-    private string _url;
+    private string? _url;
 
     public PtvApiService(IConfiguration config)
     {
-        _config = config;
-        _userID = config["DEVELOPER_CREDENTIALS:USER_ID"];
-        _apiKey = config["DEVELOPER_CREDENTIALS:API_KEY"];
-
         if (config == null)
         {
             throw new SystemException("config does not exist / is empty");
         }
-
-        if (_userID.Length == 0 || _apiKey.Length == 0)
-        {
-            throw new SystemException("USER_ID or API_KEY is empty");
-        }
+        
+        // _config = config;
+        _userId = config["DEVELOPER_CREDENTIALS:USER_ID"] ?? string.Empty;
+        _apiKey = config["DEVELOPER_CREDENTIALS:API_KEY"] ?? string.Empty;
     }
 
     
-    public string GetUrl(string request, List<(string, string)> parameters = null)
+    public string GetUrl(string request, List<(string, string)>? parameters = null)
     {
         if (parameters == null)
         {
@@ -49,12 +42,12 @@ public class PtvApiService : IPtvApiService
         }
 
         // Base URL
-        var urlHttp = "http://timetableapi.ptv.vic.gov.au";
+        // var urlHttp = "http://timetableapi.ptv.vic.gov.au";
         var urlHttps = "https://timetableapi.ptv.vic.gov.au";
         
         // Encode the api_key and message to bytes
         var keyBytes = Encoding.UTF8.GetBytes(_apiKey);
-        parameters.Add(("devid", _userID));
+        parameters.Add(("devid", _userId));
         var signatureValueParameters = HttpUtility.UrlEncode(BuildQueryString(parameters));
         signatureValueParameters = signatureValueParameters.Replace("%3d", "=");     // for human readability
         var signatureValue = $"{request}?{signatureValueParameters}";     // "The signature value is a HMAC-SHA1 hash of the completed request (minus the base URL but including your user ID, known as “devid”) and the API key"
@@ -95,39 +88,41 @@ public class PtvApiService : IPtvApiService
     public async Task<string> GetRouteTypes() // should be a Response equivalent type, not string 
     {
         string url = GetUrl("/v3/route_types");
-
-        using (HttpClient client = new HttpClient())
-        {
-            HttpResponseMessage response = await client.GetAsync(url);
-            string jsonResponse = await response.Content.ReadAsStringAsync();
-            string parsedJson = JToken.Parse(jsonResponse).ToString(Formatting.Indented);        // Parsing and Formatting the JSON Response for readability
-            return parsedJson;
-        }
+        return await FetchParsedJson(url);
     }
     
     // Gets Route Directions
-    public async Task<string> GetRouteDirections(string routeId) // should be a Response equivalent type, not string 
+    public async Task<string> GetRouteDirections(string? routeId) // should be a Response equivalent type, not string 
     {
+        if (routeId == null) {return "Route ID should not be Null";}    // Early Exit Case
         int routeIdInt = int.Parse(routeId);
         string url = GetUrl($"/v3/directions/route/{routeIdInt}");
-
-        using (HttpClient client = new HttpClient())
-        {
-            HttpResponseMessage response = await client.GetAsync(url);
-            string jsonResponse = await response.Content.ReadAsStringAsync();
-            string parsedJson = JToken.Parse(jsonResponse).ToString(Formatting.Indented);        // Parsing and Formatting the JSON Response for readability
-            return parsedJson;
-        }
+        return await FetchParsedJson(url);
     }
     
     // Getting Stops based on Location
-    public async Task<string> GetStops(string location, List<int>? routeTypes = null, int? maxResults = null, int? maxDistance = null)
+    public async Task<string> GetStops(string? location, List<int>? routeTypes = null, int? maxResults = null, int? maxDistance = null)
     {
-        var parameters = new List<(string, string)>();
-
+        if (location == null) {return "Location should not be Null";}    // Early Exit Case
+        
         string[] locations = location.Split(",");
-        double latitude = double.Parse(locations[0]);
-        double longitude = double.Parse(locations[1]);
+        if (locations.Length != 2)
+        {
+            return "Location should be in the format 'latitude,longitude'";    // Early Exit Case
+        }
+
+        if (!double.TryParse(locations[0], out double latitude))
+        {
+            return "Invalid latitude";    // Early Exit Case
+        }
+
+        if (!double.TryParse(locations[1], out double longitude))
+        {
+            return "Invalid longitude";    // Early Exit Case
+        }
+        
+        // Parameter Handling
+        var parameters = new List<(string, string)>();
 
         if (routeTypes != null)
         {
@@ -139,17 +134,88 @@ public class PtvApiService : IPtvApiService
 
         if (maxResults != null)
         {
-            parameters.Add(("max_results", maxResults.ToString()));
+            parameters.Add(("max_results", maxResults.ToString() ?? string.Empty));     // ensures that the parameter is non-empty
         }
 
         if (maxDistance != null)
         {
-            parameters.Add(("max_distance", maxDistance.ToString()));
+            parameters.Add(("max_distance", maxDistance.ToString() ?? string.Empty));
         }
 
         // PTV API request
         string url = GetUrl($"/v3/stops/location/{latitude},{longitude}", parameters);
+        return await FetchParsedJson(url);
+    }
+    
+    // // GET DEPARTURES UNEDITED
+    // public async Task<string> Departures(int routeType, int stopId, int? routeId = null, int? directionId = null, DateTime? dateUtc = null, int? maxResults = null, List<string> expand = null)
+    //     {
+    //         var parameters = new List<(string, string)>();
+    //
+    //         // Direction
+    //         if (directionId.HasValue)
+    //         {
+    //             parameters.Add(("direction_id", directionId.Value.ToString()));
+    //         }
+    //
+    //         // Date
+    //         if (dateUtc.HasValue)
+    //         {
+    //             parameters.Add(("date_utc", dateUtc.Value.ToString("yyyy-MM-ddTHH:mm:ssZ")));
+    //         }
+    //
+    //         // Max Results
+    //         if (maxResults.HasValue)
+    //         {
+    //             parameters.Add(("max_results", maxResults.Value.ToString()));
+    //         }
+    //
+    //         // Expands
+    //         if (expand != null)
+    //         {
+    //             foreach (var expandStr in expand)
+    //             {
+    //                 parameters.Add(("expand", expandStr));
+    //             }
+    //         }
+    //
+    //         // PTV API request
+    //         string url;
+    //         if (routeId.HasValue)
+    //         {
+    //             if (parameters.Count >= 1)
+    //             {
+    //                 url = GetUrl($"/v3/departures/route_type/{routeType}/stop/{stopId}/route/{routeId}", parameters);
+    //             }
+    //             else
+    //             {
+    //                 url = GetUrl($"/v3/departures/route_type/{routeType}/stop/{stopId}/route/{routeId}");
+    //             }
+    //         }
+    //         else
+    //         {
+    //             if (parameters.Count >= 1)
+    //             {
+    //                 url = GetUrl($"/v3/departures/route_type/{routeType}/stop/{stopId}", parameters);
+    //             }
+    //             else
+    //             {
+    //                 url = GetUrl($"/v3/departures/route_type/{routeType}/stop/{stopId}");
+    //             }
+    //         }
+    //         
+    //         using (HttpClient client = new HttpClient())
+    //         {
+    //             HttpResponseMessage response = await client.GetAsync(url);
+    //             string jsonResponse = await response.Content.ReadAsStringAsync();
+    //             string parsedJson = JToken.Parse(jsonResponse).ToString(Formatting.Indented);        // Parsing and Formatting the JSON Response for readability
+    //             return parsedJson;
+    //         }
+    //     }
 
+    // Maybe add a Function for the "using httpclient return parsedJson" bit
+    private async Task<string> FetchParsedJson(string url)
+    {
         using (HttpClient client = new HttpClient())
         {
             HttpResponseMessage response = await client.GetAsync(url);
@@ -159,76 +225,9 @@ public class PtvApiService : IPtvApiService
         }
     }
     
-    // GET DEPARTURES UNEDITED
-    public async Task<string> Departures(int routeType, int stopId, int? routeId = null, int? directionId = null, DateTime? dateUtc = null, int? maxResults = null, List<string> expand = null)
-        {
-            var parameters = new List<(string, string)>();
-
-            // Direction
-            if (directionId.HasValue)
-            {
-                parameters.Add(("direction_id", directionId.Value.ToString()));
-            }
-
-            // Date
-            if (dateUtc.HasValue)
-            {
-                parameters.Add(("date_utc", dateUtc.Value.ToString("yyyy-MM-ddTHH:mm:ssZ")));
-            }
-
-            // Max Results
-            if (maxResults.HasValue)
-            {
-                parameters.Add(("max_results", maxResults.Value.ToString()));
-            }
-
-            // Expands
-            if (expand != null)
-            {
-                foreach (var expandStr in expand)
-                {
-                    parameters.Add(("expand", expandStr));
-                }
-            }
-
-            // PTV API request
-            string url;
-            if (routeId.HasValue)
-            {
-                if (parameters.Count >= 1)
-                {
-                    url = GetUrl($"/v3/departures/route_type/{routeType}/stop/{stopId}/route/{routeId}", parameters);
-                }
-                else
-                {
-                    url = GetUrl($"/v3/departures/route_type/{routeType}/stop/{stopId}/route/{routeId}");
-                }
-            }
-            else
-            {
-                if (parameters.Count >= 1)
-                {
-                    url = GetUrl($"/v3/departures/route_type/{routeType}/stop/{stopId}", parameters);
-                }
-                else
-                {
-                    url = GetUrl($"/v3/departures/route_type/{routeType}/stop/{stopId}");
-                }
-            }
-            
-            using (HttpClient client = new HttpClient())
-            {
-                HttpResponseMessage response = await client.GetAsync(url);
-                string jsonResponse = await response.Content.ReadAsStringAsync();
-                string parsedJson = JToken.Parse(jsonResponse).ToString(Formatting.Indented);        // Parsing and Formatting the JSON Response for readability
-                return parsedJson;
-            }
-        }
-
-    
     // TEST
     public string GetCurrentUrl()
     {
-        return _url;
+        return _url ?? "NO URL";
     }
 }
