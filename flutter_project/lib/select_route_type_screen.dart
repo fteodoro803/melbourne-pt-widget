501cecacfc4bf8ed877e1cbd6f17a32ddc6732ea
fteodoro803 <agustinteodoro803@gmail.com>
2024-10-05 00:09:23 +1000

    Refactored to use RouteType info class

--- flutter_project/lib/select_route_type_screen.dart
+++ flutter_project/lib/select_route_type_screen.dart
@@ -1,8 +1,10 @@
 // import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'ptv_api_service.dart';
 import 'dart:convert';
 import 'selections.dart';
+import 'ptvInfoClasses/routeTypeInfo.dart';
 
 class SelectRouteTypeScreen extends StatefulWidget {
   // Constructor
@@ -17,31 +19,40 @@ class SelectRouteTypeScreen extends StatefulWidget {
 
 class _SelectRouteTypeScreenState extends State<SelectRouteTypeScreen> {
   PtvApiService apiService = PtvApiService();
-  Map<int, String> _routeTypes = {};
+  String _screenName = "SelectRouteType";
+  List<RouteType> _routeTypesClass = [];
 
+  // Initialising State
   @override
   void initState() {
     super.initState();
     fetchRouteTypes();
-    print(widget.userSelections.toString());
+
+    // Debug Printing
+    if (kDebugMode) {
+      print("Screen: $_screenName\n${widget.userSelections}");
+    }
   }
 
-  // Generates Map/Dictionary of PT Options               // I dont like how this logic is in the same file as the frontend rendering, find a way to split this
+  // Fetches Routes and generates Map/Dictionary of PT Options               // I dont like how this logic is in the same file as the frontend rendering, find a way to split this
   Future<void> fetchRouteTypes() async {
     // Fetching Data and converting to JSON
     Data data = await PtvApiService().routeTypes();
     Map<String, dynamic> jsonResponse = jsonDecode(data.response);
 
-    // Populating Map
+    // Populating RouteTypes List                                                         // add case for if 0
     for (var entry in jsonResponse["route_types"]) {
-      int routeTypeNumber = entry["route_type"];
-      String routeTypeName = entry["route_type_name"];
-      _routeTypes[routeTypeNumber] = routeTypeName;
+      RouteType newRouteType = RouteType();
+      newRouteType.type = entry["route_type"].toString();
+      newRouteType.name = entry["route_type_name"];
+
+      _routeTypesClass.add(newRouteType);
     }
 
     setState(() {});
   }
 
+  // Rendering
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,15 +60,16 @@ class _SelectRouteTypeScreenState extends State<SelectRouteTypeScreen> {
         title: Text("Select PTV:"),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        // Placeholder -- but from this point on, I want to render a clickable list of Routes from routeTypes. When clicked, these save to user preferences.
-        itemCount: _routeTypes.length,
+
+      // Generates List of Options
+      body: ListView.builder(      // old
+        itemCount: _routeTypesClass.length,
         itemBuilder: (context, index) {
           return ListTile(
-            title: Text(_routeTypes[index]!),
+            title: Text(_routeTypesClass[index].name!),
             onTap: () {
-              widget.userSelections.routeType = index;
-              widget.userSelections.routeTypeName = _routeTypes[index];
+              widget.userSelections.routeType = _routeTypesClass[index].type;
+              widget.userSelections.routeTypeName = _routeTypesClass[index].name;
               Navigator.pushNamed(context, '/selectLocationScreen');
             },
           );
