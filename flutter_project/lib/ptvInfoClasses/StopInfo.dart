eb8ad843d8e902a1c23cc69f0d16be7350498bd7
fteodoro803 <agustinteodoro803@gmail.com>
2024-10-05 00:08:15 +1000

    Classes to store Fetch Info

---
+++ flutter_project/lib/ptvInfoClasses/StopInfo.dart
@@ -0,0 +1,11 @@
+class Stop {
+  String? id;
+  String? name;
+  String? latitude;
+  String? longitude;
+  String? suburb;
+
+  String? routeId;
+  String? routeName;
+  String? routeNumber;
+}
---
+++ flutter_project/lib/ptvInfoClasses/routeTypeInfo.dart
@@ -0,0 +1,4 @@
+class RouteType {
+  String? name;      // consider changing the ? to late
+  String? type;   // 0 - train, 1 - tram, etc
+}
