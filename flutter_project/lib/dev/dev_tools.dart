b51f5b101818b718f3a121ecef8c5faa66d99178
fteodoro803 <agustinteodoro803@gmail.com>
2024-10-23 17:36:19 +1100

    created dev tools

---
+++ flutter_project/lib/dev/dev_tools.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_project/screen_arguments.dart';
+
+class DevTools {
+
+  // Prints the current Screen State
+  void printScreenState(String screenName, ScreenArguments arguments) {
+    if (kDebugMode) {
+      String transportDetails = arguments.transport.toString(); // Get the transport details as a string
+
+      // Format transport details to indent each line
+      String indentedTransportDetails = transportDetails.split('\n').map((line) => '\t\t$line').join('\n');
+
+
+      print("Screen: $screenName\n"
+        "Arguments: {\n"
+          "\tList: ${arguments.transportList}\n"
+          "\tTransport:\n$indentedTransportDetails"
+        "}");
+    }
+  }
+
+}
