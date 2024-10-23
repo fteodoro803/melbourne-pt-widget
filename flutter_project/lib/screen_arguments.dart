aea1016a642c507873a1ba7b282d94ad6a64bd9a
fteodoro803 <agustinteodoro803@gmail.com>
2024-10-23 17:36:11 +1100

    created ScreenArguments class to handle data transfer between screens

---
+++ flutter_project/lib/screen_arguments.dart
@@ -0,0 +1,12 @@
+/*
+Arguments for the addScreens
+ */
+
+import 'package:flutter_project/transport.dart';
+
+class ScreenArguments {
+  List<Transport> transportList;
+  Transport transport;
+
+  ScreenArguments(this.transportList, this.transport);
+}
