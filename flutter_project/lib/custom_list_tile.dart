fcc0c2efc2cd93eaf3621263f69fe06ee8f94d27
fteodoro803 <agustinteodoro803@gmail.com>
2025-02-11 19:28:15 +1100

    Custom List Tile updated to be stateless

--- flutter_project/lib/custom_list_tile.dart
+++ flutter_project/lib/custom_list_tile.dart
@@ -4,32 +4,17 @@ import 'utilities.dart' as utilities;
 
 class CustomListTile extends StatelessWidget {
   final Transport transport;
-  CustomListTile({super.key, required this.transport});
-
-  String? routeTypeName;
-  String? routeNumber;
-  String? directionName;
-  String? stopName;
-  String? departure1;
-  String? departure2;
-  String? departure3;
-
+  const CustomListTile({super.key, required this.transport});
 
   @override
   Widget build(BuildContext context) {
-  String? routeTypeName = transport.routeType?.name ?? "Null RouteTypeName";
-  routeNumber = transport.route?.number ?? "Null RouteNumber";
-  directionName = transport.direction?.name ?? "Null DirectionName";
-  stopName = transport.stop?.name ?? "Null StopName";
-  departure1 = "Null 1st Departure (INITIALISED)";
-  departure2 = "Null 2nd Departure (INITIALISED)";
-  departure3 = "Null 3rd Departure (INITIALISED)";
-
-  if (transport.departures != null && transport.departures!.length == 3) {
-    departure1 = utilities.getTime(transport.departures?[0].estimatedDeparture) ?? utilities.getTime(transport.departures?[0].scheduledDeparture) ?? "Null 1st Departure";
-    departure2 = utilities.getTime(transport.departures?[1].estimatedDeparture) ?? utilities.getTime(transport.departures?[1].scheduledDeparture) ?? "Null 2nd Departure";
-    departure3 = utilities.getTime(transport.departures?[2].estimatedDeparture) ?? utilities.getTime(transport.departures?[2].scheduledDeparture) ?? "Null 3rd Departure";
-  }
+    final String routeTypeName = transport.routeType?.name ?? "Null RouteTypeName";
+    final String routeNumber = transport.route?.number ?? "Null RouteNumber";
+    final String directionName = transport.direction?.name ?? "Null DirectionName";
+    final String stopName = transport.stop?.name ?? "Null StopName";
+    final String departure1 = utilities.getTime(transport.departures?[0].estimatedDeparture) ?? utilities.getTime(transport.departures?[0].scheduledDeparture) ?? "Null 1st Departure";
+    final String departure2 = utilities.getTime(transport.departures?[1].estimatedDeparture) ?? utilities.getTime(transport.departures?[1].scheduledDeparture) ?? "Null 2nd Departure";
+    final String departure3 = utilities.getTime(transport.departures?[2].estimatedDeparture) ?? utilities.getTime(transport.departures?[2].scheduledDeparture) ?? "Null 3rd Departure";
 
   return ListTile(
       isThreeLine: true,
