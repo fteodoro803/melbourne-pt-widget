c83e7122f36f0974f7e92e2ac30aab6fcb2274a5
fteodoro803 <agustinteodoro803@gmail.com>
2025-02-13 13:10:36 +1100

    Confirmation Screen now uses Custom List Tile

--- flutter_project/lib/add_screens/confirmation_screen.dart
+++ flutter_project/lib/add_screens/confirmation_screen.dart
@@ -3,7 +3,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_project/dev/dev_tools.dart';
-import 'package:flutter_project/file_service.dart';
 import 'package:flutter_project/screen_arguments.dart';
 import 'package:flutter_project/custom_list_tile.dart';
 
@@ -39,20 +38,6 @@ class _ConfirmationScreenState extends State<ConfirmationScreen> {
   @override
   Widget build(BuildContext context) {
     final transport = widget.arguments.transport;
-    final routeTypeName = transport.routeType?.name ?? "Null RouteType";
-    final routeNumber = transport.route?.number ?? "Null RouteNumber";
-    final directionName = transport.direction?.name ?? "Null DirectionName";
-    final stopName = transport.stop?.name ?? "Null StopName";
-
-    var departure1 = "Null 1st Departure (INITIALISED)";
-    var departure2 = "Null 2nd Departure (INITIALISED)";
-    var departure3 = "Null 3rd Departure (INITIALISED)";
-
-    if (transport.departures != null && transport.departures!.length == 3) {
-      departure1 = transport.departures?[0].estimatedDepartureTime ?? transport.departures?[0].scheduledDepartureTime ?? "Null 1st Departure (SET)";
-      departure2 = transport.departures?[1].estimatedDepartureTime ?? transport.departures?[1].scheduledDepartureTime ?? "Null 2nd Departure (SET)";
-      departure3 = transport.departures?[2].estimatedDepartureTime ?? transport.departures?[2].scheduledDepartureTime ?? "Null 3rd Departure (SET)";
-    }
 
     return Scaffold(
       appBar: AppBar(
@@ -60,18 +45,8 @@ class _ConfirmationScreenState extends State<ConfirmationScreen> {
         centerTitle: true,
       ),
 
-      // Generates List of Stops
-      body: ListTile(
-        isThreeLine: true,
-        title: Text("$routeTypeName $routeNumber to $directionName"),
-        subtitle: Text("from $stopName\n"
-            "$departure1 | $departure2 | $departure3"),
-        onTap: () async {
-          await append(widget.arguments.transport);
-          widget.arguments.callback();    // calls the screen arguments callback function
-          Navigator.popUntil(context, ModalRoute.withName("/"));
-        },
-      ),
+      // Generates Example of Stop
+      body: CustomListTile(transport: transport),
     );
   }
 }
