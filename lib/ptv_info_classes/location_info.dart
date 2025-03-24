9056083e5c599b275e45773d54cf7efe3e8963d2
fteodoro803 <agustinteodoro803@gmail.com>
2025-03-24 18:04:33 +1100

    toLatLng() functionality for location

--- lib/ptv_info_classes/location_info.dart
+++ lib/ptv_info_classes/location_info.dart
@@ -1,5 +1,6 @@
 // idk if the app needs to store this? maybe at most get the users current location to get nearest stops, but delete after~
 
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'location_info.g.dart';
@@ -7,13 +8,33 @@ part 'location_info.g.dart';
 @JsonSerializable()
 class Location {
   String location;
-  // latitude and longitude?
+  double latitude;
+  double longitude;
 
-  Location({required this.location});
+  Location({required this.location}) : latitude = 0, longitude = 0 {
+    // Split the location string by comma (assuming "latitude,longitude" format)
+    List<String> parts = location.split(',');
+
+    // Ensure there are exactly two parts (latitude and longitude)
+    if (parts.length == 2) {
+      latitude = double.parse(parts[0].trim()); // Parse latitude from string
+      longitude = double.parse(parts[1].trim()); // Parse longitude from string
+    } else {
+      latitude = 0;
+      longitude = 0;
+      throw FormatException(
+          '( location_info.dart -> constructor ) -- Invalid location format');
+    }
+  }
 
   @override
   String toString() {
-    return "Location: $location\n";
+    return "Location: $location\t"
+        "Latitude, Longitude = $latitude, $longitude";
+  }
+
+  LatLng toLatLng() {
+    return LatLng(latitude, longitude);
   }
 
   // Methods for JSON Serialization
