97618270b80bd0fbd6766e1ca8de9a5896648db5
fteodoro803 <agustinteodoro803@gmail.com>
2025-03-31 20:53:55 +1100

    location_info testing

--- test/SAMPLE.dart
+++ test/SAMPLE.dart
@@ -7,19 +7,39 @@
     - Given optional arguments, are they ignored/included when Inputted
     - Status code = 200 (works)?
   - Api call-specific (v3/routeTypes, v3/route/directions, etc)
+
+  - Groupings
+    - Valid Input for Constructors
+    - Invalid Input for Constructors
+    - Edge Cases for Constructors
+    - Class Methods   (can be more lax with these because we are "assured" correct formatting with the constructors' tests)
 * */
 
-// import 'package:flutter_test/flutter_test.dart';
+/*
+import 'package:flutter_test/flutter_test.dart';
 
-// main() {
-//   test('Calculator should add two numbers correctly', () {
-// // Arrange: Set up the Calculator instance
-//     final calculator = Calculator();
-//
-// // Act: Call the method being tested
-//     final result = calculator.add(2, 3);
-//
-// // Assert: Check if the result is as expected
-//     expect(result, equals(5));
-//   });
-// }
+
+// Arrange: Set up the Calculator instance
+// Act: Call the method being tested
+// Assert: Check if the result is as expected
+
+
+group("Valid Input", () {
+  // Setup for common values
+  late String name;
+
+  setUp(() {
+    name = "nameOfThing";
+  });
+
+  // Tests
+  test("Valid values"), () {
+
+  });
+
+});
+
+group("Invalid Input", () {});
+group("Edge Cases", () {});
+
+ */
