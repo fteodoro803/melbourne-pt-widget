ca82538490674f9d8f4290aa5d32709b68a41e82
fteodoro803 <agustinteodoro803@gmail.com>
2025-04-30 16:07:05 +1000

    created gtfs api fetch functions

--- lib/api/gtfs_api_service.dart
+++ lib/api/gtfs_api_service.dart
@@ -3,11 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:gtfs_realtime_bindings/gtfs_realtime_bindings.dart';
 
 class GtfsApiService {
-  final String
-      subscriptionKey; // todo: store as an environment variable or something
-  final String serviceAlertsUrl = "https://data-exchange-api.vicroads.vic.gov.au/opendata/gtfsr/v1/tram/servicealert";
-  final String tripUpdatesUrl = "https://data-exchange-api.vicroads.vic.gov.au/opendata/gtfsr/v1/tram/tripupdates";
-  final String vehiclePositionsUrl = "https://data-exchange-api.vicroads.vic.gov.au/opendata/gtfsr/v1/tram/vehicleposition";
+  final String subscriptionKey; // todo: store as an environment variable or something
 
   /// Creates a GtfsApiService object, with an optional GlobalConfiguration
   GtfsApiService({GlobalConfiguration? config})
@@ -22,9 +18,10 @@ class GtfsApiService {
 
   // Fetch tram trip updates
   Future<FeedMessage> getTramTripUpdates() async {
+    final String tramTripUpdatesUrl = "https://data-exchange-api.vicroads.vic.gov.au/opendata/gtfsr/v1/tram/tripupdates";
     try {
       final response = await http.get(
-        Uri.parse(tripUpdatesUrl),
+        Uri.parse(tramTripUpdatesUrl),
         headers: _headers,
       );
       
@@ -41,30 +38,79 @@ class GtfsApiService {
     }
   }
 
+  // Fetch tram service alerts
+  Future<FeedMessage> getTramServiceAlerts() async {
+    final String serviceAlertsUrl = "https://data-exchange-api.vicroads.vic.gov.au/opendata/gtfsr/v1/tram/servicealert";
+    try {
+      final response = await http.get(
+        Uri.parse(serviceAlertsUrl),
+        headers: _headers,
+      );
+
+      if (response.statusCode == 200) {
+        // Parsing protobuf response
+        final feedMessage = FeedMessage.fromBuffer(response.bodyBytes);
+        return feedMessage;
+      }
+      else {
+        throw Exception("( gtfs_api_service.dart -> getTramServiceAlerts ) -- Failed to load trip updates: ${response.statusCode}");
+      }
+    } catch(e) {
+      throw Exception("( gtfs_api_service.dart -> getTramServiceAlerts ) -- Error fetching service alerts $e");
+    }
+  }
+
+  // Fetch vehicle positions
+  Future<FeedMessage> getTramVehiclePositions() async {
+    final String vehiclePositionsUrl = "https://data-exchange-api.vicroads.vic.gov.au/opendata/gtfsr/v1/tram/vehicleposition";
+    try {
+      final response = await http.get(
+        Uri.parse(vehiclePositionsUrl),
+        headers: _headers,
+      );
+
+      if (response.statusCode == 200) {
+        // Parsing protobuf response
+        final feedMessage = FeedMessage.fromBuffer(response.bodyBytes);
+        return feedMessage;
+      }
+      else {
+        throw Exception("( gtfs_api_service.dart -> getTramVehiclePositions ) -- Failed to load vehicle positions: ${response.statusCode}");
+      }
+    } catch(e) {
+      throw Exception("( gtfs_api_service.dart -> getTramVehiclePositions ) -- Error fetching vehicle positions $e");
+    }
+  }
+
   /// Get Updates for a specific tram route via GTFS
   // todo: move this to ptv_service
   Future<List<TripUpdate>> getTripUpdatesRoute(String routeId) async {
     final feedMessage = await getTramTripUpdates();
 
-    int maxCount = 2;
-    int count = 0;
-
-    for (var entity in feedMessage.entity) {
-      if (count>=2) {
-        break;
-      }
-
-      if (entity.hasTripUpdate()) {
-        var newEntity = entity.tripUpdate;
-        print(entity);
-      }
-      count++;
-    }
+    // for (var entity in feedMessage.entity) {
+    // }
 
     return feedMessage.entity
         .where((entity) => entity.hasTripUpdate())          // filters entities to trips with tripUpdates
         .map((entity) => entity.tripUpdate)                 // turns all those entities to tripUpdates
         .where((update) => update.trip.routeId == routeId)  // filters all tripUpdates to those with same routeId
         .toList();                                          // turns it into a list
+  }
+
+  Future<void> getTramAlert(String id) async {
+    final feedMessage = await getTramServiceAlerts();
+
+    print(feedMessage);
+  }
+
+  Future<void> getTramPosition(String id) async {
+    final feedMessage = await getTramVehiclePositions();
+
+    // for (var entity in feedMessage.entity) {
+    //   // if (entity.vehicle.trip.tripId)
+    //   print(entity);
+    // }
+
+    print(feedMessage);
   }
 }
