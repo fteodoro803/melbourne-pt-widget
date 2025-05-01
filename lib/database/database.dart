fd027e8dd652058d216f1be843b50d79b6592a5c
fteodoro803 <agustinteodoro803@gmail.com>
2025-05-01 16:14:27 +1000

    added batch inserts functionality

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:drift/drift.dart';
 import 'package:drift_flutter/drift_flutter.dart';
 import 'package:path_provider/path_provider.dart';
@@ -313,6 +314,27 @@ class AppDatabase extends _$AppDatabase {
     }
     else {
       await (update(table)..where(whereClause)).write(newData);
+    }
+  }
+
+  /// Generic method to batch insert a list of entries to a table.
+  // todo: find a way to add mergeUpdate here
+  Future<void> batchInsert<T extends Table, D extends DataClass>(
+      TableInfo<T, D> table,
+      List<Insertable<D>> entries,
+      {int batchSize = 100}
+    ) async {
+
+    // Process batches in chunks
+    for (int i=0; i < entries.length; i+=batchSize) {
+
+      // If current index plus batchSize is less than total entries, the final/end index for this current batch is the current largest
+      int end = min((i+batchSize), entries.length);
+      var currBatch = entries.sublist(i, end);
+
+      await batch((b) {
+        b.insertAllOnConflictUpdate(table, currBatch);
+      });
     }
   }
 }
