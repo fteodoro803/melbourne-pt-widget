330b887bf8f2a5dce925a523df5044edb2977bf0
fteodoro803 <agustinteodoro803@gmail.com>
2025-05-02 17:06:53 +1000

    Gets tram positions based on route ID

--- lib/services/gtfs_service.dart
+++ lib/services/gtfs_service.dart
@@ -1,15 +1,12 @@
 import 'dart:io';
 
-import 'package:drift/drift.dart';
 import 'package:flutter_project/api/gtfs_api_service.dart';
 import 'package:flutter_project/database/database.dart' as db;
 import 'package:flutter_project/database/helpers/gtfs_route_helpers.dart';
 import 'package:flutter_project/database/helpers/gtfs_trip_helpers.dart';
 import 'package:flutter_project/database/helpers/route_helpers.dart';
-import 'package:flutter_project/domain/route.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:csv/csv.dart';
 
 /* GTFS Steps
@@ -123,43 +120,41 @@ class GtfsService {
     return mapList;
   }
 
-  // todo:
+  /// Returns a list of LatLng objects representing the current positions of trams on a specified route.
   // todo: map ptv routeId to gtfs routeId by name (shortname then longname)
   // todo: edge case, combined routes (501-503)
-  // fixme: works functionally, but the results from the api dont match the dataset
-  Future<void> getTramPositions(int routeId) async {
+  Future<List<LatLng>> getTramPositions(int routeId) async {
     List<LatLng> locations = [];
     final feedMessage = await gtfsApiService.tramVehiclePositions();
     String? gtfsRouteId;
 
-    // Maps PTV route id to GTFS' route id
+    // 1. Map PTV route ID to GTFS route ID
     var ptvRouteData = await database.getRouteById(routeId);
     if (ptvRouteData != null) {
       var ptvRoute = ptvRouteData.toCompanion(true);
       var gtfsRoute = await database.getGtfsRouteFromPtvRoute(ptvRoute, "tram");
 
       gtfsRouteId = gtfsRoute?.routeId;
-      print("( gtfs_service.dart -> getTramPositions ) -- \n\tptvRoute: $ptvRouteData, \n\tgtfsRoute: $gtfsRoute");
+      // print("( gtfs_service.dart -> getTramPositions ) -- \n\tptvRoute: $ptvRouteData, \n\tgtfsRoute: $gtfsRoute");
     }
 
-    // Go through trips table and get trips with the routeId
+    // 2. Get GTFS route IDs associated with the route
     List<db.GtfsTripsTableData>? trips = gtfsRouteId != null ? await database.getGtfsTripsByRouteId(gtfsRouteId) : null;
-    List<String>? tripIds = trips?.map((t) => t.tripId).toList();
-    // print(tripIds);
+    List<String>? tripIds = trips?.map((t) => t.tripId).toList();     // Converts trips to tripIds
 
+    // 3. Collect positions of vehicles
     if (tripIds != null) {
       for (var entity in feedMessage.entity) {
         if (tripIds.contains(entity.vehicle.trip.tripId)) {
-          print(entity);
+          double latitude = entity.vehicle.position.latitude;
+          double longitude = entity.vehicle.position.longitude;
+          LatLng newLocation = LatLng(latitude, longitude);
+          locations.add(newLocation);
         }
       }
     }
 
-    // for (var entity in feedMessage.entity) {
-    //   print(entity.vehicle);
-    // }
-
-    // print(feedMessage);
+    return locations;
   }
 
   Future<void> getTramTripUpdates() async {
