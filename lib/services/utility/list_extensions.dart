d12205f100afad08243d4c895089a1b0b69b1b6e
fteodoro803 <agustinteodoro803@gmail.com>
2025-05-25 22:13:36 +1000

    Generalised sharedSublist
    Took 26 minutes

--- lib/services/utility/list_extensions.dart
+++ lib/services/utility/list_extensions.dart
@@ -17,29 +17,32 @@ extension ListUtils<E> on List<E> {
     });
   }
 
-  /// Returns the longest continuous sublist starting from [index] where all elements
+  /// Returns the longest continuous sublist starting from [item] where all elements
   /// are also present in [otherList].
-  List<E> sharedSublist(List<E> otherList, int index) {
+  List<E> sharedSublist(List<E> otherList, E item) {
     // Edge cases
-    if (index > length || index < 0|| isEmpty || otherList.isEmpty || this.hasSharedItems(otherList) == false) {
+    if (isEmpty || otherList.isEmpty || this.hasSharedItems(otherList) == false) {
       return [];
     }
 
-    // Convert otherList to Set for efficiency
+    // Convert to Set for efficiency
     Set<E> otherSet = Set.from(otherList);
 
     // 1. Check if selected element in this list is in the other list
-    if (!otherSet.contains(this[index])) {
+    if (!otherSet.contains(item)) {
       return [];
     }
 
     // Assumption: All elements in a list is unique
-    // todo: what if there's one element in the list? Or what if the index is at the start/end of the list
+    int index = this.indexOf(item);
     int minIndex = index;
     int maxIndex = index;
 
+    // Case if item doesn't exist in list
+    if (index == -1) return [];
+
     // 2. Check left side
-    for (int i=index; i>0; i--) {
+    for (int i=index; i>=0; i--) {
       // print("${this[i]} in $otherSet: ${otherSet.contains(this[i])}");
       if (otherSet.contains(this[i])) {
         // print("minIndex = $i");
@@ -63,7 +66,7 @@ extension ListUtils<E> on List<E> {
     // 4. Create a sublist of this list, from minIndex to maxIndex
     List<E> sublist = this.sublist(minIndex, maxIndex+1);
     // print("( list_extensions.dart -> sharedSublist ) -- Sublist($minIndex, $maxIndex) of $this:\n\t$sublist");
-    
+
     return sublist;
   }
 }
