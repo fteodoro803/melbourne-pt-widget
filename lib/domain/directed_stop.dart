1930130ffb52895259d55a7ae8da1128bbe88f6c
fteodoro803 <agustinteodoro803@gmail.com>
2025-06-03 19:04:55 +1000

    added test ideas
    Took 11 minutes

--- lib/domain/directed_stop.dart
+++ lib/domain/directed_stop.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_project/domain/stop.dart';
 import 'package:flutter_project/domain/trip.dart';
+// TODO(TEST): Check if all trips go towards the same direction
 
 class DirectedStop {
   List<Trip> trips;
@@ -12,6 +13,7 @@ class DirectedStop {
   String toString() {
     String str = "";
     str += "Directed Stop: Routes [${trips.map((t) => t.route?.id).toList()}] at Stop ${stop.id} towards $direction";
+    // str += "Trips towards ${trips.map((t) => t.direction?.name).toList()}";
     return str;
   }
 }
