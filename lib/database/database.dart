d0f2e5a51b9a13d9c3cc969a988cc66794995882
fteodoro803 <agustinteodoro803@gmail.com>
2025-09-20 16:13:49 +1000

    moved database table functions to database helpers
    Took 16 minutes

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,6 +1,6 @@
-import 'dart:math';
 import 'package:drift/drift.dart';
 import 'package:drift_flutter/drift_flutter.dart';
+import 'package:flutter_project/database/helpers/database_helpers.dart';
 import 'package:path_provider/path_provider.dart';
 
 part 'database.g.dart';
@@ -349,43 +349,4 @@ class AppDatabase extends _$AppDatabase {
   //     await delete(departures).go();
   //   }
 
-  /// Generic method to merge and update records.
-  /// Only updates fields that are present in the new data.
-  // todo: lots of tests on this one, and the cases where it inserts new row, updates a field, does nothing, etc
-  Future<void> mergeUpdate<T extends Table, D>(
-      TableInfo<T, D> table,
-      Insertable<D> newData,
-      Expression<bool> Function(T) whereClause,
-      ) async {
-    final query = select(table)..where(whereClause);
-    final existing = await query.getSingleOrNull();
-
-    if (existing == null) {
-      await into(table).insertOnConflictUpdate(newData);
-    }
-    else {
-      await (update(table)..where(whereClause)).write(newData);
-    }
-  }
-
-  /// Generic method to batch insert a list of entries to a table.
-  // todo: find a way to add mergeUpdate here
-  Future<void> batchInsert<T extends Table, D extends DataClass>(
-      TableInfo<T, D> table,
-      List<Insertable<D>> entries,
-      {int batchSize = 150}
-    ) async {
-
-    // Process batches in chunks
-    for (int i=0; i < entries.length; i+=batchSize) {
-
-      // If current index plus batchSize is less than total entries, the final/end index for this current batch is the current largest
-      int end = min((i+batchSize), entries.length);
-      var currBatch = entries.sublist(i, end);
-
-      await batch((b) {
-        b.insertAllOnConflictUpdate(table, currBatch);
-      });
-    }
-  }
 }
--- lib/database/helpers/gtfs_trip_helpers.dart
+++ lib/database/helpers/gtfs_trip_helpers.dart
@@ -1,5 +1,6 @@
 import 'package:drift/drift.dart' as drift;
 import 'package:flutter_project/database/database.dart';
+import 'package:flutter_project/database/helpers/database_helpers.dart';
 
 extension GtfsTripHelpers on AppDatabase {
   GtfsTripsTableCompanion createGtfsTripCompanion({required String tripId, required String routeId, required String shapeId, required String tripHeadsign, required int wheelchairAccessible}) {
