47f07185950ad3207b137f2799f3551cac47b58a
fteodoro803 <agustinteodoro803@gmail.com>
2025-12-03 19:23:23 +1100

    addRoute function changed to use companion

--- lib/dev/add_screens_old/select_stop_screen.dart
+++ lib/dev/add_screens_old/select_stop_screen.dart
@@ -84,12 +84,15 @@ class _SelectStopScreenState extends State<SelectStopScreen> {
     String routeNumber = _routes[index].number;
     // String gtfsId = _routes[index].gtfsId;
     String status = _routes[index].status;
-    database.routesDao.addRoute(
+
+    var dbRoute = database.routesDao.createRouteCompanion(
         id: routeId,
         name: routeName,
         number: routeNumber,
         routeTypeId: routeTypeId,
-        status: status);
+        status: status
+    );
+    database.routesDao.addRoute(dbRoute);
 
     int stopId = _stops[index].id;
     String stopName = _stops[index].name;
